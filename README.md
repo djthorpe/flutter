f539a852bdd5d5f2add621053bf034e9efc3ceb4
David Thorpe <djt@mutablelogic.com>
2019-09-28 13:47:17 +0100

    Updated reviewapp

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # flutter
+
 Flutter application examples
+
+* __ReviewApp__ Example from [State management for Flutter apps with MobX](https://circleci.com/blog/state-management-for-flutter-apps-with-mobx/) (CircleCI Tutorials)
+
---
+++ reviewapp/models/review.dart
@@ -0,0 +1,21 @@
+import 'package:meta/meta.dart';
+
+class ReviewModel {
+  final String comment;
+  final int stars;
+  const ReviewModel({@required this.comment, @required this.stars});
+
+  factory ReviewModel.fromJson(Map<String, dynamic> parsedJson) {
+    return ReviewModel(
+      comment: parsedJson['comment'],
+      stars: parsedJson['stars'],
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return {
+      'comment': this.comment,
+      'stars': this.stars,
+    };
+  }
+}
