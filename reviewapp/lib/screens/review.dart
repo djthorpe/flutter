775d16cfee53a6a82e0e74ba947a94cc61be8f8d
David Thorpe <djt@mutablelogic.com>
2019-09-28 13:57:04 +0100

    Updated reveiwapp changes

---
+++ reviewapp/lib/screens/review.dart
@@ -0,0 +1,128 @@
+
+import 'package:flutter/material.dart';
+import '../widgets/infocard.dart' show InfoCard;
+
+class Review extends StatefulWidget {
+  @override
+  ReviewState createState() {
+    return new ReviewState();
+  }
+}
+
+class ReviewState extends State<Review> {
+  final List<int> _stars = [1, 2, 3, 4, 5];
+  final TextEditingController _commentController = TextEditingController();
+  int _selectedStar;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double screenWidth = screenSize.width;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Review App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 12.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  width: screenWidth * 0.6,
+                  child: TextField(
+                    controller: _commentController,
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(10),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      hintText: "Write a review",
+                      labelText: "Write a review",
+                    ),
+                  ),
+                ),
+                Container(
+                  child: DropdownButton(
+                    hint: Text("Stars"),
+                    elevation: 0,
+                    value: _selectedStar,
+                    items: _stars.map((star) {
+                      return DropdownMenuItem<int>(
+                        child: Text(star.toString()),
+                        value: star,
+                      );
+                    }).toList(),
+                    onChanged: (item) {
+                      setState(() {
+                        _selectedStar = item;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  child: Builder(
+                    builder: (BuildContext context) {
+                      return IconButton(
+                        icon: Icon(Icons.done),
+                        onPressed: () {},
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 12.0),
+            //contains average stars and total reviews card
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                InfoCard(
+                    infoValue: '2',
+                    infoLabel: "reviews",
+                    cardColor: Colors.green,
+                    iconData: Icons.comment),
+                InfoCard(
+                  infoValue: '2',
+                  infoLabel: "average stars",
+                  cardColor: Colors.lightBlue,
+                  iconData: Icons.star,
+                  key: Key('avgStar'),
+                ),
+              ],
+            ),
+            SizedBox(height: 24.0),
+            //the review menu label
+            Container(
+              color: Colors.grey[200],
+              padding: EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Icon(Icons.comment),
+                  SizedBox(width: 10.0),
+                  Text(
+                    "Reviews",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            //contains list of reviews
+            Expanded(
+              child: Container(
+                child: Text("No reviews yet"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ reviewapp/lib/widgets/infocard.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class InfoCard extends StatelessWidget {
+  final String infoValue;
+  final String infoLabel;
+  final Color cardColor;
+  final IconData iconData;
+  const InfoCard(
+      {Key key,
+      @required this.infoValue,
+      @required this.infoLabel,
+      @required this.cardColor,
+      @required this.iconData,
+      })
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+    double screenWidth = screenSize.width;
+    return Container(
+      height: 100,
+      width: screenWidth / 2,
+      child: Card(
+        color: cardColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Icon(
+              iconData,
+              size: 28.0,
+              color: Colors.white,
+            ),
+            Text(
+              "$infoValue $infoLabel",
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
