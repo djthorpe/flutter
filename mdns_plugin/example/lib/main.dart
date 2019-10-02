5062cb706575f3590e38f2ff23bd7923c0b00d40
David Thorpe <djt@mutablelogic.com>
2019-10-02 11:10:19 +0100

    Updated screenshot

--- mdns_plugin/example/lib/main.dart
+++ mdns_plugin/example/lib/main.dart
@@ -69,6 +69,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        debugShowCheckedModeBanner: false,
         home: Scaffold(
             appBar: AppBar(
               title: const Text('Chromecast Browser'),
