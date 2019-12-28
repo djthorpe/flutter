c2c0f5798f514862d0fe8e80ed589c86bf9c0ec9
David Thorpe <djt@mutablelogic.com>
2019-12-28 12:10:45 +0000

    Updates

---
+++ mdns_browser/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <zeroconf/ZeroconfPlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [ZeroconfPlugin registerWithRegistrar:[registry registrarForPlugin:@"ZeroconfPlugin"]];
+}
+
+@end
